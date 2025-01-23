eea2012122ceca8b8c90e7c5f4656f1acecfffbd
Caleb John <johncaleb022@gmail.com>
2025-01-23 10:51:00 +0100

    Update constants.ts

--- next-app/src/app/constants.ts
+++ next-app/src/app/constants.ts
@@ -6,7 +6,6 @@
 
 
 
-
 import { createThirdwebClient, getContract } from "thirdweb";
 import { baseSepolia, defineChain } from "thirdweb/chains";
 import { inAppWallet, SmartWalletOptions } from "thirdweb/wallets";
